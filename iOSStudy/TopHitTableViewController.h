3a8e263c69ed7983c81442d8f89a404d54bc01a2
PC19 <admin@pc19.local>
2015-12-10 19:22:51 +0700

    - simple table view

---
+++ iOSStudy/TopHitTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopHitTableViewController.h
+//  iOSStudy
+//
+//  Created by PC19 on 12/10/15.
+//  Copyright (c) 2015 OrganizationName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopHitTableViewController : UITableViewController
+
+@end
