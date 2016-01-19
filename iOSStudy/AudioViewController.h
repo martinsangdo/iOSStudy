b727957871aec6e15fe5bec9d552a4ce28eb5a49
PC19 <admin@pc19.local>
2016-01-19 19:04:36 +0700

    play audio (not completed)

---
+++ iOSStudy/AudioViewController.h
@@ -0,0 +1,17 @@
+//
+//  AudioViewController.h
+//  iOSStudy
+//
+//  Created by PC19 on 1/19/16.
+//  Copyright (c) 2016 OrganizationName. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface AudioViewController : ViewController{
+    
+}
+- (IBAction)playAudio:(id)sender;
+
+@end
